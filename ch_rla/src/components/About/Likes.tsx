86e1b10649851fe5d85260b3df3fcc88a2c39ca0
PiperChang <goddhqj8000@ajou.ac.kr>
2022-07-21 16:11:32 +0900

    Feat: "img following mouse"

--- ch_rla/src/components/About/Likes.tsx
+++ ch_rla/src/components/About/Likes.tsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Likes.module.css';
 
 export default function Likes() {
+  const list = [
+    {
+      title: 'The Hangover',
+      author: 'Todd Phillips',
+      img: require('../../asset/Likes/hangover.jpg'),
+      year: '2009',
+    },
+    {
+      title: 'Joker',
+      author: 'Todd Phillips',
+      img: require('../../asset/Likes/joker.jpg'),
+      year: '2019',
+    },
+    {
+      title: 'The Bird That Drinks Blood',
+      author: 'Lee Young-do',
+      img: require('../../asset/Likes/피마새.jpg'),
+      year: '2005',
+    },
+    {
+      title: 'Night Visions',
+      author: 'Imagine Dragons',
+      img: require('../../asset/Likes/Night Visions.jpg'),
+      year: '2012',
+    },
+    {
+      title: 'Barbarian Quest',
+      author: '백수귀족',
+      img: require('../../asset/Likes/Barbarian Quest.jpg'),
+      year: '2012',
+    },
+    {
+      title: 'SKT (Swallow Knight Tales)',
+      author: 'Kim Chul-gon',
+      img: require('../../asset/Likes/SKT.jpg'),
+      year: '2003',
+    },
+    {
+      title: 'One Is Not a Lonely Word',
+      author: 'Drunken Tiger',
+      img: require('../../asset/Likes/dt.jpg'),
+      year: '2004',
+    },
+  ];
+
+  const [displayOn, setDisplayOn] = useState(-1);
+
+  // mouse position
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    // get mouse coordinates
+    const handleWindowMouseMove = (event: MouseEvent) => {
+      setCoords({
+        x: event.screenX,
+        y: event.screenY,
+      });
+    };
+    window.addEventListener('mousemove', handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleWindowMouseMove);
+    };
+  }, []);
+
+  const likeslist = list.map((like, idx) => (
+    <div
+      key={idx}
+      onMouseEnter={() => setDisplayOn(idx)}
+      onMouseLeave={() => setDisplayOn(-1)}
+    >
+      {displayOn == idx ? <img src={like.img} style={{left:(coords.x + 400), top:coords.y + 50}}/> : <></>}
+      <h3>{like.title}</h3>
+      <p>
+        {like.author}, {like.year}
+      </p>
+    </div>
+  ));
+
   return (
-    <article className={styles.Container}>
-      <h2>Movies</h2>
-      <h3>Hangover</h3>
-    </article>
+    <>
+      <article className={styles.Container}>{likeslist}</article>
+    </>
   );
 }
---
+++ ch_rla/src/components/About/Profile.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import profile from '../../asset/profile/profile.jpg';
+import { AiFillInstagram } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
+import { FaGithubAlt } from 'react-icons/fa';
+import styles from './Profile.module.css'
+export default function Profile() {
+  return (
+    <article className={styles.Profile}>
+    <section>
+      <img src={profile} alt="Profile picture" />
+      <div className={styles.Intro}>
+        <strong>Kim Chang Hui </strong>
+        <br />
+        is a software engineer based on Korea.
+        <br />
+        <br />
+        He loves many things. You&apos;ll
+        see what he likes.
+        <br />
+        <br />
+        Hope you enjoy it too.
+      </div>
+    </section>
+    <section className={styles.Contact}>
+      <a href="mailto: goddhqj8000@ajou.ac.kr">
+        <MdEmail size={'2rem'} />
+      </a>
+      <a href="https://github.com/PiperChang">
+        <FaGithubAlt size={'2rem'} />
+      </a>
+      <a href="https://www.instagram.com/ch_rla/">
+        <AiFillInstagram size={'2rem'} />
+      </a>
+    </section>
+  </article>
+  )
+}
