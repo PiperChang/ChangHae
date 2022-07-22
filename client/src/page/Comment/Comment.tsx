8046e05de8e65d50b3fa4fe9a9eaad873952cac8
PiperChang <goddhqj8000@ajou.ac.kr>
2022-07-22 22:46:31 +0900

    Fix: "Typing error"

--- client/src/page/Comment/Comment.tsx
+++ client/src/page/Comment/Comment.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import axios
+import axios from 'axios'
+
 function Comment() {
-  
-  
   return (
     <main>If you have visited here. Please leave me any comment. So that I can be better myself</main>
-
   )
 }
 
